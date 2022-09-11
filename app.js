7de196f28df55cce4092b8489f16c3576bf869b4
SaracensFan <matthewclifford@hotmail.co.uk>
2022-09-11 09:02:51 +0100

    update

--- app.js
+++ app.js
@@ -10,22 +10,39 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+const posts = []
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get('/', (req, res) => {
+  res.render('home', { posts: posts })
+  console.log(posts);
+})
 
+app.get('/about', (req, res) => {
+  res.render('about', { aboutContent: aboutContent })
+})
 
+app.get('/contact', (req, res) => {
+  res.render('contact', { contactContent: contactContent })
+})
 
+app.get('/compose', (req, res) => {
+  res.render('compose')
+})
 
-
-
-
-
-
-
+app.post('/compose', (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  }
+  posts.push(post)
+  res.redirect('/')
+})
 
 
 app.listen(3000, function() {
